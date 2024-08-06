910bef9b3609872b03e94f1f652651fe10d37f29
Raj Kumar <rajkrjsr17@gmail.com>
2024-08-06 23:35:35 +0530

    Add create activity endpoint

--- routes/activity.js
+++ routes/activity.js
@@ -2,10 +2,23 @@ var express = require('express');
 var router = express.Router();
 var activityService = require('../service/activityService.js')
 
+// TODO: Consolidate error handling
 /* Return all activities */
 router.get('/', async function(req, res, next) {
     const activities = await activityService.listActivities()
     res.status(200).json(activities);
+});
+
+router.post('/', async function(req, res, next) {
+    const { title, description, category, duration, difficultyLevel, content } = req.body;
+
+    try {
+        const newActivity = await activityService.createActivity(title, description, category, duration, difficultyLevel, content);
+        res.status(201).json({ newActivity });
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ error: error.message || 'An error occurred while creating the activity' });
+    }
 });
 
 router.post('/complete/:activityId', async function(req, res, next) {
--- service/activityService.js
+++ service/activityService.js
@@ -1,11 +1,34 @@
 const Activity = require('../models/activity.js');
 const { generateFixedActivities } = require('./defaultActivities.js');
 activities = generateFixedActivities();
+let nextId = activities.length ? Math.max(...activities.map(a => a.id)) + 1 : 1; // Generate the next ID
 const userActivityMap = new Map();
 
 exports.listActivities = async () => {
     return activities;
 };
+
+exports.createActivity = async (title, description, category, duration, difficultyLevel, content) => {
+    if (!title || !description || !category || !duration || !difficultyLevel || !content) {
+        throw new Error('All fields are required');
+    }
+
+    const newActivity = {
+        id: nextId++,
+        title,
+        description,
+        category,
+        duration,
+        difficultyLevel,
+        content,
+        creationTimestamp: new Date()
+    };
+
+    activities.push(newActivity);
+
+    return newActivity;
+};
+
 
 exports.markCompleted = async (activityId, userId) => {
 
@@ -24,10 +47,10 @@ exports.markCompleted = async (activityId, userId) => {
 
 exports.getCompletedActivities = async (userId) => {
     if (!userActivityMap.has(userId)) {
-        return []; // No completed activities for the user
+        return [];
     }
 
-    // Retrieve completed activities for the user
+
     const completedActivityIds = Array.from(userActivityMap.get(userId));
     const completedActivities = activities.filter(activity => completedActivityIds.includes(activity.id));
     return completedActivities;
