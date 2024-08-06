3892c1e17b2b536efda9d9ea59e590b83b907448
Raj Kumar <rajkrjsr17@gmail.com>
2024-08-06 21:50:51 +0530

    Move request handling to controller

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var userService = require('../service/userService.js')
+var User = require('../models/user.js');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -9,7 +11,15 @@ router.get('/', function(req, res, next) {
 
 /* Register new user */
 router.post('/register', function(req, res, next) {
-   userService.register(req, res)
+    const { username, email, password } = req.body;
+    try {
+        let user = new User(username, email, password)
+        userService.register(user)
+        res.status(201).json({ user });
+    } catch (error) {
+    console.log(error)
+      res.status(400).json({ error: 'Registration failed' });
+    }
 });
 
 /* Login user */
